92e8f20dbb1d1601c748ea2dcf46b0ed99d2a86a
Fábio Ueno <fabio.t.ueno@gmail.com>
2022-12-29 08:27:02 -0300

    feat(results): Create results server

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -8,13 +8,11 @@ defmodule Duper.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Duper.Worker.start_link(arg)
-      # {Duper.Worker, arg}
+      Duper.Results
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Duper.Supervisor]
+
     Supervisor.start_link(children, opts)
   end
 end
